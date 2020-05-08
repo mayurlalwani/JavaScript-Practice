9b41a696d17673749601ba7f39b074d010d3cf7f
mayurlalwani <mayur_lalwani4@ymail.com>
2020-05-08 15:13:11 +0530

    First Commit

---
+++ Triangle Loop/triangleLoop.js
@@ -0,0 +1,12 @@
+function looping_a_triangle() {
+    let hashes = "";
+    for (let i = 0; i < 7; i++) {
+      hashes += "#";
+      console.log(hashes);
+    }
+  }
+
+looping_a_triangle();
+
+
+  
